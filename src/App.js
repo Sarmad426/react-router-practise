9a89f2f3ffe1b8c28b714ac1f7a509e1d87ab9e3
Sarmad Rafique <sarmadrafique040@gmail.com>
2022-10-18 22:35:25 +0500

    Added Custom Routes:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./pages/about";
+import { Home } from "./pages/home";
+import Navbar from "./pages/navbar";
+import Products from "./pages/products";
+import Error from "./pages/error";
+import SingleProduct from "./pages/singleProduct";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="products/:productId" element={<SingleProduct />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

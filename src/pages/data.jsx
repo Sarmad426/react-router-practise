d9dff07fbdd7e9efcdade6570dac314d82b569e6
Sarmad Rafique <sarmadrafique040@gmail.com>
2022-10-18 22:44:02 +0500

    Added a data array:

---
+++ src/pages/data.jsx
@@ -0,0 +1,22 @@
+export const productData = [
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaU2cAziT7cNLfxYJ3Z_fZUuSHi0-Ntu0Dlg&usqp=CAU",
+    name: "Shirt No.1",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp01Qni8zvvdFIgJAyy2n0f1ByDdVxvy4Fgg&usqp=CAU",
+    name: "Shirt No.2",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3pHYQ4A8p8TwL8Vg-nfIVmPEB932_4WH3Mg&usqp=CAU",
+    name: "Shirt No.3",
+  },
+  {
+    id: 4,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG3XkzWfGBkWUJklSLNn4GGgShg7RWuXH0og&usqp=CAU",
+    name: "Shirt No.4",
+  },
+];

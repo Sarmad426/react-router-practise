5609fba80a96addbbf11ca3203998d0aeb9f108d
Sarmad Rafique <sarmadrafique040@gmail.com>
2022-10-19 15:14:13 +0500

    Added more routes  to increase functionality:

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import Navbar from "./pages/navbar";
 import Products from "./pages/products";
 import Error from "./pages/error";
 import SingleProduct from "./pages/singleProduct";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import React from "react";
+import ProtectedRoute from "./pages/protectedRoute";
 function App() {
+  const [user, setUser] = React.useState(null);
   return (
     <BrowserRouter>
       <Navbar />
@@ -15,6 +20,15 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="products" element={<Products />} />
         <Route path="products/:productId" element={<SingleProduct />} />
+        <Route path="login" element={<Login setUser={setUser} />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute user={user}>
+              <Dashboard user={user} />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>

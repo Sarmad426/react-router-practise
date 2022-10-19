c6dfb3dd54b9b039368c00fa102fb5c42fe0a5c6
Sarmad Rafique <sarmadrafique040@gmail.com>
2022-10-19 15:18:07 +0500

    Added a Login page link:

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Link to="login" className="btn">
+        Login
+      </Link>
     </div>
   );
 };

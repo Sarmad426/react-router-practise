9b1323f0c96195bb5fb726efbd46b8377332e078
Sarmad Rafique <sarmadrafique040@gmail.com>
2022-10-19 15:16:20 +0500

    Added price for every product:

--- src/pages/data.jsx
+++ src/pages/data.jsx
@@ -3,20 +3,24 @@ export const productData = [
     id: 1,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaU2cAziT7cNLfxYJ3Z_fZUuSHi0-Ntu0Dlg&usqp=CAU",
     name: "Shirt No.1",
+    price: 30,
   },
   {
     id: 2,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp01Qni8zvvdFIgJAyy2n0f1ByDdVxvy4Fgg&usqp=CAU",
     name: "Shirt No.2",
+    price: 35,
   },
   {
     id: 3,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3pHYQ4A8p8TwL8Vg-nfIVmPEB932_4WH3Mg&usqp=CAU",
     name: "Shirt No.3",
+    price: 20,
   },
   {
     id: 4,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG3XkzWfGBkWUJklSLNn4GGgShg7RWuXH0og&usqp=CAU",
     name: "Shirt No.4",
+    price: 40,
   },
 ];
